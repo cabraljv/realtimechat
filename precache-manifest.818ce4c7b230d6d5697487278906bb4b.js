e4425393b051f0b42cd3b8584a15f341eabccf5f
cabraljv <joaovictorcabral.dev@gmail.com>
2020-09-23 18:37:34 -0300

    Updates

---
+++ precache-manifest.818ce4c7b230d6d5697487278906bb4b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104d7d631327987277c487c5d3c2802b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37baa7c75a5accbec092",
+    "url": "/static/css/main.ee18194c.chunk.css"
+  },
+  {
+    "revision": "849f1ffa6193b8adb601",
+    "url": "/static/js/2.01cfabf4.chunk.js"
+  },
+  {
+    "revision": "2549765df2eb04fa59a1f28130514f65",
+    "url": "/static/js/2.01cfabf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37baa7c75a5accbec092",
+    "url": "/static/js/main.d0c4f651.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "e0655dee3606f0dd07772211af6917fd",
+    "url": "/static/media/accept.e0655dee.svg"
+  },
+  {
+    "revision": "08fce901a09b10c3662f20a31a8d5916",
+    "url": "/static/media/add_contact.08fce901.svg"
+  },
+  {
+    "revision": "c779daafe07927fe3d882408158012d1",
+    "url": "/static/media/add_contact_blue.c779daaf.svg"
+  },
+  {
+    "revision": "26c7410808351f89cd69ee51508af470",
+    "url": "/static/media/logo.26c74108.svg"
+  },
+  {
+    "revision": "98669d692f8d9ed42513669a3c07adb6",
+    "url": "/static/media/logout.98669d69.svg"
+  },
+  {
+    "revision": "e0a407f54b6abf4339c8f1f70ae3b5fa",
+    "url": "/static/media/plus .e0a407f5.svg"
+  },
+  {
+    "revision": "5263b13e8b7bebda99234db12a596ba9",
+    "url": "/static/media/send.5263b13e.svg"
+  }
+]);
